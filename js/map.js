452746ba24af0bf0ece7c7c748eb118beae4e329
cborde <cborde414@gmail.com>
2020-12-22 11:47:06 +0100

    Ajout de la carte

---
+++ js/map.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(_e) {
+	
+	function onMapClick(e) {
+		alert("You clicked the map at " + e.latlng);
+	}
+
+	/*
+	 * MAIN
+	 */ 
+	var map = L.map('mapid').setView([47.23, 6.02], 12);
+	
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © OpenStreetMap contributors';
+	var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+	
+	map.addLayer(osm);
+	
+	var marker = L.marker([47.23, 6.02]).addTo(map);
+	marker.addEventListener("click", function(){
+		marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+	});
+	
+	map.on('click', onMapClick);
+});
